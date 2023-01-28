800e42d0f38f3a59b99b23a9f2e315bedc0ccc3e
achicyr <hi.cyril@gmail.com>
2023-01-28 20:12:26 +0900

    DONE GEM SIMPLE_FORM => GESTION DES FORMULAIRES vidéo n° 23 : https://www.youtube.com/watch?v=67ZnlWo4wtI&list=PLjwdMgw5TTLWfI1B2Wv2WPgR9iOyw12zi&index=23 pour observer le travail concerner, regarder les fichiers: - app/views/devise/registrations/edit.html.erb (concernant le model User associé avec Devise donc) - app/views/posts/_form.html.erb (concernant le model Post donc)

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module GemDevise
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.i18n.available_locales = :en
+    config.i18n.default_locale = :fr
   end
 end
